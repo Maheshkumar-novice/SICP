3ecafa4661dc4a5cd49ac4a10eaa6a1d99c7fa33
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-11-07 12:43:49 +0530

    Update 1.3.scm

--- Chapter-1/exercises/1.3.scm
+++ Chapter-1/exercises/1.3.scm
@@ -27,3 +27,31 @@
 )
 
 (sum_of_squares_of_large_two 1 3 4)
+
+# Another approach
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y z)
+  	(cond (
+           (and (> x z) (> y z))
+           (+ (square x) (square y)) 
+  		  )
+          (
+           (and (> x y) (> z y))
+           (+ (square x) (square z)) 
+  		  )
+          (
+           (and (> y x) (> z x))
+           (+ (square y) (square z)) 
+  		  )
+    )
+)
+
+(sum-of-squares 1 2 3)
+(sum-of-squares 1 3 2)
+(sum-of-squares 2 3 1)
+(sum-of-squares 3 2 1)
+(sum-of-squares 2 1 3)
+(sum-of-squares 3 1 2)
+(sum-of-squares 100 20 3)
